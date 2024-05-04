d3a75ba70417aecdc844aa121a2c155744bf54e1
GhostViz <joe@fjoenichols.com>
2024-05-04 03:03:25 -0600

    mvp

--- a_mountain_ale/a_mountain_ale/urls.py
+++ a_mountain_ale/a_mountain_ale/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("blog.urls")),
 ]
+
+admin.site.site_header = "A Mountain Ale Admin"
+admin.site.site_title = "A Mountain Ale Admin Portal"
+admin.site.index_title = "Welcome to A Mountain Ale Admin Portal"
